0967a07add73c9f2fe7b08dd4b9859283445999a
Yoshikuni Kato <yoshikuni.kato.g@gmail.com>
2017-03-04 15:13:33 +0900

    Update DetailViewController transition

--- radihey/RoomViewController.swift
+++ radihey/RoomViewController.swift
@@ -7,8 +7,12 @@
 //
 
 import UIKit
+import AVFoundation
 
 final class RoomViewController: UIViewController {
+    
+    var firebaseClient: FirebaseClient!
+    var channelName: String!
     
     @IBOutlet weak var buttonsBackgroundView: UIView!
     
@@ -32,7 +36,21 @@ final class RoomViewController: UIViewController {
         ReactionButtonCellModel(title: "へぇ"),
         ReactionButtonCellModel(title: "いいね"),
     ]
+
     
+    
+    /**
+     - SeeAlse: http://xyk.hatenablog.com/entry/2017/02/19/002743
+     */
+    func playSound(voiceType: VoiceStamp) {
+        let url = voiceType.getSound()
+        var soundID: SystemSoundID = 0
+        AudioServicesCreateSystemSoundID(url as CFURL, &soundID)
+        AudioServicesAddSystemSoundCompletion(soundID, nil, nil, { (soundID, _) in
+            AudioServicesDisposeSystemSoundID(soundID)
+        }, nil)
+        AudioServicesPlaySystemSound(soundID)
+    }
     
     // MARK: Lifecycle
     override func viewDidLoad() {
@@ -40,6 +58,14 @@ final class RoomViewController: UIViewController {
         buttonsBackgroundView.layer.shadowColor = UIColor(rgb: 0x432A17).cgColor
         buttonsBackgroundView.layer.shadowOpacity = 0.5
         buttonsBackgroundView.layer.shadowOffset = CGSize(width: 0.0, height: -1.0)
+        
+        var first = true
+        firebaseClient.addReactionObserver(
+        of: channelName) { [weak self] reaction in
+            if first { first = false; return }
+            guard let voiceType = reaction.voiceType else { return }
+            self?.playSound(voiceType: voiceType)
+        }
     }
 }
 
